bd6ae232cea349986862978bfb7f09cc0b4de2a3
Marcus Longmuir <marcus@mpl.dev>
2023-12-01 14:24:15 +0000

    Fix publish script working directory

--- publish/publish.sh
+++ publish/publish.sh
@@ -3,8 +3,13 @@
 # Exit on error and echo each command
 set -ex
 
+# Set the working directory to the location of this script
+cd "$(dirname "$0")"
+
+# Create the build directory
 mkdir -p ./build/tools
 
+# Copy the index.html file that references the different tools into the build directory
 cp ./index.html ./build/index.html
 
 # Copy the files for each tool into the build directory
